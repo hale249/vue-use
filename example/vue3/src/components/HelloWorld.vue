273ae56011ac85a82805fcd0d433700f605ecf80
hale249 <lehatybg1@gmail.com>
2022-12-22 16:06:43 +0700

    fix: 🐛 fix issue export module

---
+++ example/vue3/src/components/HelloWorld.vue
@@ -0,0 +1,26 @@
+<template>
+  <p class="read-the-docs">isPendingSpinner: {{ isPendingSpinner }}</p>
+</template>
+
+<script lang="ts">
+import {ref} from "vue";
+import {useSpinner, useTitle} from "@mirawayltd/vue-use";
+
+export default {
+  setup() {
+    const isPending = ref(true);
+    useTitle('Miraway vue use title')
+    const isPendingSpinner = useSpinner(isPending, 300)
+    return {
+      isPendingSpinner
+    }
+  },
+
+}
+</script>
+
+<style scoped>
+.read-the-docs {
+  color: #888;
+}
+</style>
